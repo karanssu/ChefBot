c50062f34c9f1a2453f503ba5ee06f7b40cba7ec
karanssu <s0377009salemstate@gmail.com>
2024-09-19 11:20:26 -0400

    terminated the setup

--- server.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const path = require('path');
-const srcPath = path.resolve(__dirname, 'src');
-
-// Dynamically require the main entry point
-require(path.join(srcPath, 'index'));
