a0f642e5212472de3768a4e4fa20c9e5db587cfb
karanssu <s0377009salemstate@gmail.com>
2024-09-19 12:28:26 -0400

    commants updated in ask.js

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-const express = require("express"); 
-const app = express(); 
-const cors = require("cors"); 
- 
-require("dotenv").config(); 
-const SERVER_PORT = process.env.SERVER_PORT || 8000; 
- 
-app.use(cors({ origin: "*" })); 
-app.use(express.json()); 
- 
-// user 
-app.use("/ask", require("./routes/ask")); 
- 
-app.listen(SERVER_PORT, (req, res) => { 
-    console.log( 
-        `Backend Server is running on port ${SERVER_PORT}...` 
-    ); 
-}); 
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+require("dotenv").config();
+const SERVER_PORT = process.env.SERVER_PORT || 8000;
+
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+
+// ask
+app.use("/ask", require("./routes/ask"));
+
+app.listen(SERVER_PORT, (req, res) => {
+  console.log(`Backend Server is running on port ${SERVER_PORT}...`);
+});
